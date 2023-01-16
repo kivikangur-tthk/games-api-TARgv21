ed55f1f786f2a2351a63a5ad96439acedca4fb13
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-01-16 15:23:02 +0200

    Add database connection to env use database in app

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+require("dotenv").config()
 const app = require("express")()
-const port = 8080
+const mysql = require("mysql")
+const port = process.env.APP_PORT
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_BASE
+})
+
+connection.connect(function(err){
+    if(err) throw err
+    connection.query("SELECT * FROM customers",
+        function(err,result,fields){
+            if(err) throw err
+            console.log(result)
+        })
+})
+
 
 app.listen(port,()=>{
     console.log(`API up at: http://localhost:${port}`)
