d19860921210cd5eb33eb86d79ea1b4376fd8243
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-01-16 17:16:52 +0200

    Add endpoint to show all customers using mariadb

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 require("dotenv").config()
 const app = require("express")()
-const mysql = require("mysql")
+const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
-const connection = mysql.createConnection({
+const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_BASE
+    database: process.env.DB_BASE,
+    connectionLimit: 5
 })
 
-connection.connect(function(err){
-    if(err) throw err
-    connection.query("SELECT * FROM customers",
-        function(err,result,fields){
-            if(err) throw err
-            console.log(result)
-        })
+app.get("/customers",async (req,res)=>{
+    let conn
+    try {
+        conn = await pool.getConnection()
+        const rows = await conn.query("SELECT firstname, lastname FROM customers")
+        res.send(JSON.stringify(rows))
+    } catch (error) {
+        console.log(error)       
+    } finally{
+        conn.end()
+    }
 })
-
 
 app.listen(port,()=>{
     console.log(`API up at: http://localhost:${port}`)
