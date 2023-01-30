1079d836b5f9e1dce01bdfe094d1b8abf57aa8f7
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2023-01-30 16:18:26 +0200

    Add create game and associated methods [delivers #184347802]

--- models/Game.model.js
+++ models/Game.model.js
@@ -9,6 +9,13 @@ module.exports = (sequelize,Sequelize) => {
             type: Sequelize.STRING,
             allowNull: false
         },
+        description:{
+            type: Sequelize.STRING(4096)
+        },
+        genre:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
         studio: {
             type: Sequelize.STRING
         },
