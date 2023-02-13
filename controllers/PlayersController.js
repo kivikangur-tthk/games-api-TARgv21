00eadcc42b40b57dfa921afce289644b8bcbc24e
Kristjan Kivikangur <Kristjan.Kivikangur@tthk.ee>
2023-02-13 15:06:41 +0200

    Add player details endpoint implementation [delivers #184237775]

--- controllers/PlayersController.js
+++ controllers/PlayersController.js
@@ -1,19 +1,29 @@
 const { db } = require("../db")
 const Players = db.players
+const GamePlays = db.gamePlays
 
 exports.getAll = async (req,res)=>{
     const players = await Players.findAll({attributes:["id","firstName"]})
     if (players.length == 0){
         res.send({"message":"No players exist"})
-    } else {
-        res.send(JSON.stringify(players))
-    }    
+  } else {
+    res.send(JSON.stringify(players))
+  }
 }
 exports.createNew = async (req,res)=>{    
     res.send({"message":"Not ipmlemented yet"})
 }
-exports.getById = async (req,res)=>{    
-    res.send({"message":"Not ipmlemented yet"})
+exports.getById = async (req, res) => {
+  const player = await Players.findByPk(req.params.id, {
+    logging: console.log,
+    include: GamePlays,
+    order: [[GamePlays,"GameId","DESC"]]
+  })
+  if (player === null) {
+    res.status(404).send({ error: "Player not found" })
+  } else {
+    res.send(player)
+  }
 }
 exports.updateById = async (req,res)=>{    
     res.send({"message":"Not ipmlemented yet"})
