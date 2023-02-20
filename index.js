75e44d307b657df6b6a6ff1cbbce2fe8fff6c0d9
Kristjan Kivikangur <Kristjan.Kivikangur@tthk.ee>
2023-02-20 18:07:24 +0200

    Add cors and configure it to accept requests from front-end project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const mariadb = require("mariadb")
 const port = process.env.APP_PORT
 
@@ -10,6 +11,11 @@ const yamljs = require("yamljs")
 const swaggerDocument = yamljs.load("./docs/swagger.yaml")
 app.use("/docs", swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 app.use(express.json())
+const corsOptions = {
+    origin:"http://localhost:5173"
+}
+
+app.use(cors(corsOptions))
 require("./routes/app_routes")(app)
 
 
